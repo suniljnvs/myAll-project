f7fb024bbb9fe30fce47263eee23adf3ffeaa170
suniljnvs <sunilkumar0551998@gmail.com>
2022-04-29 00:18:39 +0530

    add login controller

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const authorController = require("../controllers/authorController");
 const blogController = require("../controllers/blogController");
+const loginController = require("../controllers/loginController");
 
 router.post("/authors", authorController.createAuthor);
 router.post("/blogs", blogController.createBlog);
@@ -12,5 +13,6 @@ router.put("/blogs/:blogId", blogController.updateBlog);
 
 router.delete("/blogs/:blogId", blogController.deleteBlog);
 router.delete("/blogs", blogController.deleteBlogsQueryParams);
+router.post("/login", loginController.loginAuthor);
 
 module.exports = router;
