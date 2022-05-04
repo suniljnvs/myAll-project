73acb238373105bf922d84be89f79e77343968ab
suniljnvs <sunilkumar0551998@gmail.com>
2022-05-04 22:51:01 +0530

    my project1-Blog

--- src/models/authorModel.js
+++ src/models/authorModel.js
@@ -1,30 +1,28 @@
 const mongoose = require("mongoose");
 
-const authorSchema = new mongoose.Schema(
-  {
+const authorSchema = new mongoose.Schema({
     fname: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
     lname: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
     title: {
-      type: String,
-      required: true,
-      enum: ["Mr", "Mrs", "Miss"],
+        type: String,
+        required: true,
+        enum: ["Mr", "Mrs", "Miss"],
     },
     email: {
-      type: String,
-      required: true,
-      unique: true,
+        type: String,
+        required: true,
+        unique: true,
     },
     password: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
-  } //{ timestamps: true }
-);
+}, { timestamps: true });
 
 module.exports = mongoose.model("Author", authorSchema);
